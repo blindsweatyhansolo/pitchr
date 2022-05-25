5ba70a90ef9e562ea6a0060ebc70732d58544eea
J Semidey <mtsemidey@gmail.com>
2022-05-24 20:03:52 -0400

    server set up

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,16 @@
 // SEQUELIZE SERVER CONNECTION //
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    }
+);
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+// handlebars setup
+const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+// const session = require('express-session');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const sess = {
+//     secret: process.env.SECRET,
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
+
+// // helpers section placeholder //
+// const helpers = require('./utils/helpers');
+// const hbs = exphbs.create({ helpers });
+
+
+// middleware
+// app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// middleware for static assests in 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// // handlebars set up
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// turn on routes
+// app.use(routes);
+
+// connection to db and server
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log("Database connected . . .");
+    app.listen(PORT, () => console.log(`NOW LISTENING ON PORT ${PORT}`));
+});
