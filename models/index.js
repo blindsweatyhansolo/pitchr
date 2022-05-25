7b1e8b576180624dc5563fe413e4b9f6ffa8fabc
J Semidey <mtsemidey@gmail.com>
2022-05-25 15:40:29 -0400

    created ProjectKeyword model to allow association through this model between Project and Keyword

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 // IMPORT ALL MODEL FILES TO SET ASSOCIATIONS
 const Project = require('./Project');
 const Vote = require('./Vote');
+const Keyword = require('./Keyword');
+const ProjectKeyword = require('./ProjectKeyword');
 
-module.exports = { Project, Vote };
+module.exports = { Project, Vote, Keyword, ProjectKeyword };
