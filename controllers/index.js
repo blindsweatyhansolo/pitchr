53ee63a00012245da7ad87ec694fce6fd5bb8dad
Sam Miller <samdakota@gmail.com>
2022-05-26 19:34:58 -0400

    created comment model and CRUD routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,86 @@
-// COLLECTS PACKAGED API ROUTES FROM CONTROLLERS/API/INDEX.JS //
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then()
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Comment.findOne({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'text',
+            'CreatedAt',
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username'],
+            },
+            {
+                model: post,
+                attributes: ['id', 'username'],
+            }
+        ]
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    Comment.create({
+        text: req.body.text,
+        post_id: req.body.post_id,
+        creator_id: req.session.user_id
+    })
+    ..then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        res.status(500).json(err);
+    });
+})
+
+module.exports = router;
+
+// GET all Comments
+// Include: User model [username] / Post model [id, name]
+// GET single Comment
+
+// Use findOne() or findByPk() method
+// Attributes:
+// ID
+// Text
+// CreatedAt
+// Include:
+// User model [username]
+// Post model [id, name]
+// POST new Comment
+
+// Use create() method
+// Required params:
+// Text
+// Post ID
+// User ID (use id from session: req.session.user_id)
+// DELETE single Comment [ /:id ]
+
+// Use destroy()
