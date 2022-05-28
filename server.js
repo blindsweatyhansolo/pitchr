4127d9e308c60f579a5fc167c4c3d5748f37e762
J Semidey <mtsemidey@gmail.com>
2022-05-28 13:11:06 -0400

    set up handlebars in server, main layout

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // // handlebars set up
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
