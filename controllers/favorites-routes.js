e9864d39723e0659b95ecdacf7b29572973c8c2b
J Semidey <mtsemidey@gmail.com>
2022-05-31 18:24:46 -0400

    fixed syntax error on edit page route, was set as PUT instead of GET

--- controllers/favorites-routes.js
+++ controllers/favorites-routes.js
@@ -26,22 +26,9 @@ router.get("/", async (req, res) => {
   });
 });
 
-// router.get("/:id", async (req, res) => {
-//   // const user = await User.findOne({
-//   //   where: {
-//   //     id: req.params.id,
-//   //   },
-//   // });
-
-//   if (!req.session.loggedIn) {
-//     res.redirect("login");
-//   } else {
-//     res.render("favorites", { loggedIn: true });
-//   }
-// });
 
 
-router.put('/edit-project/:id', (req, res) => {
+router.get('/edit-project/:id', (req, res) => {
   // IF LOGGED IN USER IS PROJECT CREATOR THEN ALLOW EDIT
   // OTHERWISE ALERT/MODAL CAN'T EDIT
   Project.findOne({
@@ -76,43 +63,6 @@ router.put('/edit-project/:id', (req, res) => {
     console.log(err);
     res.status(500).json(err);
   });
-
-  // Project.update(req.body, {
-  //     individualHooks: true,
-  //     where: {
-  //         id: req.params.id
-  //     }
-  //   })
-  // .then(dbUserData => {
-  //   if (!dbUserData) {
-  //       res.status(404).json({ message: 'No user found with this id!'})
-  //       return;
-  //   }
-  //   res.json({ message: 'Password updated!'});  
-  //   })   
-  // .catch(err => {
-  //   console.log(err);
-  //   res.status(500).json({ message: 'Failed to successfully create Project !'});
-  //   });
 });
 
-// router.delete("/:?", async (req, res) => {
-//   // delete a category by its `id` value
-//   await Project.destroy({
-//     where: {
-//       id: req.params.id,
-//     },
-//   });
-//   res.status(200).json({ message: 'Deleted.' });
-// });
-
-
-// const projects = dbProjectData.map((project) => project.get({ plain: true }));
-// const project = dbProjectData.get({ plain: true });
-
 module.exports = router;
-
-
-
-
-//pass projects voted on to favorites page and render that list based on that argument. 
